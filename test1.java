6641c6cb7194f7f3fb3303dc0a185c084727cb4e
FujiitsuK <cmin1731865@gmail.com>
2024-08-03 23:49:38 +0900

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,19 @@
+package study;
+
+/**
+ * 转义字符
+ * @author min
+ *
+ */
+public class test1 {
+	public static void main(String [] args) {
+		// 转义字符:用于转义的符号是:\
+		System.out.println("转义字符测试");
+		System.out.print("我想输出一个回车符\n\r");
+		System.out.print("\t这是一个制表符");
+		System.out.println("\\");
+		System.out.println("\"双引号\"");
+		System.out.println("\'单引号\'");
+		
+	}
+}
