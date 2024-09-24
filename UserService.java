d7633d58ed3642af26373e40e60d5038de4d36d5
Chennnn <cmin1731865@gmail.com>
2024-09-24 23:25:51 +0900

    Add files via upload

---
+++ UserService.java
@@ -0,0 +1,18 @@
+package com.cm.ex;
+
+public class UserService {
+	
+	public User login(String username,String password) throws MyException {
+		
+		//排错法
+		if (!"admin".equals(username)){
+			throw new MyException("用户名错误");
+		}
+		
+		if (!"12345".equals(password)) {
+			throw new MyException("密码错误");
+		}
+		User user = new User("admin","12345",18,"男");
+		return user;
+	}
+}
