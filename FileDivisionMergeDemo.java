ebb1132aa31790204a341839f25967b3499f8a0b
Chennnn <cmin1731865@gmail.com>
2024-10-21 00:32:40 +0900

    Add files via upload

--- FileDivisionMergeDemo.java
+++ FileDivisionMergeDemo.java
@@ -5,6 +5,10 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.util.Enumeration;
+import java.util.Vector;
 
 public class FileDivisionMergeDemo {
 
@@ -71,14 +75,87 @@ public class FileDivisionMergeDemo {
 	 * 文件合并
 	 * @param args
 	 */
-	private static void merge() {
+	private static void merge(Enumeration<InputStream> es) {
+		//构造一个合并流
+		SequenceInputStream sis = new SequenceInputStream(es);
+		try {
+			BufferedOutputStream bos = new BufferedOutputStream(
+					new FileOutputStream("c:\\1-temp-5月2日.mp4"));
+			
+			byte[] bytes = new byte[1024];
+			int len =-1;
+			while ((len = sis.read(bytes))!= -1) {
+				bos.write(bytes,0,len);
+				bos.flush();
+			}
+			bos.close();
+			sis.close();
+			System.out.println("合并完成");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
 	public static void main(String[] args) {
 
-		File file = new File("C:\\Users\\Administrator\\Videos\\5月2日\\5月2日.mp4");
-		division(file, 1024*1024*20);
+//		File file = new File("C:\\Users\\Administrator\\Videos\\5月2日\\5月2日.mp4");
+//		division(file, 1024*1024*20);
+		
+		try {
+			InputStream in1 = new FileInputStream(new File("C:\\1-temp-5月2日.mp4"));
+			InputStream in2 = new FileInputStream(new File("C:\\2-temp-5月2日.mp4"));
+			InputStream in3 = new FileInputStream(new File("C:\\3-temp-5月2日.mp4"));
+			InputStream in4 = new FileInputStream(new File("C:\\4-temp-5月2日.mp4"));
+			InputStream in5 = new FileInputStream(new File("C:\\5-temp-5月2日.mp4"));
+			InputStream in6 = new FileInputStream(new File("C:\\6-temp-5月2日.mp4"));
+			InputStream in7 = new FileInputStream(new File("C:\\7-temp-5月2日.mp4"));
+			InputStream in8 = new FileInputStream(new File("C:\\8-temp-5月2日.mp4"));
+			InputStream in9 = new FileInputStream(new File("C:\\9-temp-5月2日.mp4"));
+			InputStream in10 = new FileInputStream(new File("C:\\10-temp-5月2日.mp4"));
+			InputStream in11 = new FileInputStream(new File("C:\\11-temp-5月2日.mp4"));
+			InputStream in12 = new FileInputStream(new File("C:\\12-temp-5月2日.mp4"));
+			InputStream in13 = new FileInputStream(new File("C:\\13-temp-5月2日.mp4"));
+			InputStream in14 = new FileInputStream(new File("C:\\14-temp-5月2日.mp4"));
+			InputStream in15 = new FileInputStream(new File("C:\\15-temp-5月2日.mp4"));
+			InputStream in16 = new FileInputStream(new File("C:\\16-temp-5月2日.mp4"));
+			InputStream in17 = new FileInputStream(new File("C:\\17-temp-5月2日.mp4"));
+			InputStream in18 = new FileInputStream(new File("C:\\18-temp-5月2日.mp4"));
+			InputStream in19 = new FileInputStream(new File("C:\\19-temp-5月2日.mp4"));
+			InputStream in20 = new FileInputStream(new File("C:\\20-temp-5月2日.mp4"));
+			InputStream in21 = new FileInputStream(new File("C:\\21-temp-5月2日.mp4"));
+			
+			//集合工具类,内部实现使用了数组
+			Vector<InputStream> v = new Vector<InputStream>();
+			v.add(in1);
+			v.add(in2);
+			v.add(in3);
+			v.add(in4);
+			v.add(in5);
+			v.add(in6);
+			v.add(in7);
+			v.add(in8);
+			v.add(in9);
+			v.add(in10);
+			v.add(in11);
+			v.add(in12);
+			v.add(in13);
+			v.add(in14);
+			v.add(in15);
+			v.add(in16);
+			v.add(in17);
+			v.add(in18);
+			v.add(in19);
+			v.add(in20);
+			v.add(in21);
+			
+			Enumeration<InputStream> es = v.elements();
+			merge(es);
+			} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
