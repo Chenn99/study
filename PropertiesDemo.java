fdebb92aa709d7b9ad91234f9d8e79b8b7cd9fa6
Chennnn <cmin1731865@gmail.com>
2024-10-22 22:33:39 +0900

    Add files via upload

---
+++ PropertiesDemo.java
@@ -0,0 +1,21 @@
+
+/**
+ * Properties:
+ * Properties可以用来做配置文件
+ * javaweb和javaee 开发中通常会用到
+ * 
+ * @author Administrator
+ *
+ */
+public class PropertiesDemo {
+
+	
+	private static String version = "";
+	private static String username = "admin";
+	private static String password = "123";
+	
+	public static void main(String[] args) {
+
+	}
+
+}
