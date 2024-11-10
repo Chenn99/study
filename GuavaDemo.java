c8031c6b9ada0cf3192562b539a8bf843aaf5031
Chennnn <cmin1731865@gmail.com>
2024-11-10 17:49:28 +0900

    Add files via upload

--- GuavaDemo.java
+++ GuavaDemo.java
@@ -4,7 +4,11 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import org.junit.Test;
@@ -12,12 +16,107 @@ import org.junit.Test;
 import com.google.common.base.Function;
 import com.google.common.base.Functions;
 import com.google.common.base.Preconditions;
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.BiMap;
 import com.google.common.collect.Collections2;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.HashBiMap;
+import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multimap;
 import com.google.common.collect.Sets;
+import com.google.common.collect.Table;
+import com.google.common.collect.Table.Cell;
 import com.google.common.collect.Sets.SetView;
 
 public class GuavaDemo {
+	
+	/**
+	 * 双键的Map -> Table -> rowKey+columnKey+value
+	 */
+	@Test
+	public void testGuava10() {
+		Table<String, String, Integer> table = HashBasedTable.create();
+		table.put("jack", "java", 80);
+		table.put("tom", "php", 70);
+		table.put("bin", "java", 59);
+		table.put("lily", "c", 98);
+		
+		Set<Cell<String, String, Integer>> cells = table.cellSet();
+		for (Cell cell : cells) {
+			System.out.println(cell.getRowKey()+"-"+cell.getColumnKey()+"-"+cell.getValue());
+//			jack-java-80
+//			tom-php-70
+//			bin-java-59
+//			lily-c-98
+		}
+	}
+	
+	/**
+	 * BiMap:双向Map（bidirectional Map）键与值不能重复
+	 */
+	@Test
+	public void testGuava9() {
+		BiMap<String, String> map = HashBiMap.create();
+		map.put("finally_test", "17313628537");
+		map.put("bin_test", "19824748264");
+		String name = map.inverse().get("19824748264");
+		System.out.println(name);
+		
+		//System.out.println(map.inverse().inverse()==map);//true
+		//inverse 代表反转，将key 和 value 的位置调换
+	}
+	
+	/**
+	 * MultiMap:key可重复
+	 */
+	@Test
+	public void testGuava8() {
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("Java从入门到精通", "tom");
+		map.put("Android从入门到精通", "tom");
+		map.put("PHP从入门到精通", "jack");
+		map.put("天天开心", "vince");
+		
+		Multimap<String, String> mmap = ArrayListMultimap.create();
+		Iterator<Map.Entry<String, String>> iter = map.entrySet().iterator();//加一个迭代器
+		while (iter.hasNext()) {
+			Map.Entry<String, String> entry = iter.next();
+			mmap.put(entry.getValue(), entry.getKey());
+		}
+		
+		Set<String> keySet = mmap.keySet();
+		for (String key : keySet) {
+			Collection<String> val = mmap.get(key);
+			System.out.println(key+"->"+val);
+		}
+//		tom->[Java从入门到精通, Android从入门到精通]
+//		vince->[天天开心]
+//		jack->[PHP从入门到精通]
+	}
+	
+	/**
+	 * Multise:无序可重复
+	 */
+	@Test
+	public void testGuava7() {
+		String s = "good good study day day up";
+		String[] ss = s.split(" ");
+		HashMultiset<String> set = HashMultiset.create();
+		for (String str : ss) {
+			set.add(str);
+		}
+		
+		Set<String> set2 = set.elementSet();
+		for (String str : set2) {
+			System.out.println(str+":"+set.count(str));
+		}
+		//输出结果
+//		study:1
+//		up:1
+//		good:2
+//		day:2
+	}
 	
 	/**
 	 * 集合操作：交集，差集，并集
