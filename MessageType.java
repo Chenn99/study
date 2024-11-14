559bad36486965bcd2ed0b5e9109e59cca5201db
Chennnn <cmin1731865@gmail.com>
2024-11-14 23:37:16 +0900

    Add files via upload

---
+++ MessageType.java
@@ -0,0 +1,7 @@
+package com.louis.communication;
+
+public  final class MessageType{
+    public static final int TYPE_LOGIN = 0x1; //登录消息的类型
+    public static final int TYPE_SEND = 0x2; //发送消息的类型
+
+}
