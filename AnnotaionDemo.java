aa5230698e45bc1b457f9bef6cab0220b1cba549
Chennnn <cmin1731865@gmail.com>
2024-11-30 22:27:58 +0900

    Add files via upload

--- AnnotaionDemo.java
+++ AnnotaionDemo.java
@@ -8,14 +8,41 @@ import java.util.List;
 //消除警告信息
 @SuppressWarnings("all")
 public class AnnotaionDemo {
-    @Test
-    public void test(){
-        Cat cat = new Cat("喵喵","3");
-        cat.printInfo();//调用一个已过时的方法
 
-        List list = new ArrayList();
-        list.add("louis");
-        list.add(10);
-        list.add(10.1f);
+    //反射来处理注解
+    @Test
+    public void test2(){
+        Class<Cat> catClass = Cat.class;
+        //获取类上应用的指定注解
+        MyAnnotation annotation = catClass.getAnnotation(MyAnnotation.class);
+        //获取注解上变量值
+        String name = annotation.name();
+        String age = annotation.age();
+        Color1 color1 = annotation.color1();
+        String[] like = annotation.like();
+        try {
+            Cat cat = catClass.newInstance();
+            cat.setName(name);
+            cat.setAge(age);
+            cat.setColor1(color1);
+            cat.setLike(like);
+            System.out.println(cat);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+
     }
+
+//    @Test
+//    public void test(){
+//        Cat cat = new Cat("喵喵","3");
+//        cat.printInfo();//调用一个已过时的方法
+//
+//        List list = new ArrayList();
+//        list.add("louis");
+//        list.add(10);
+//        list.add(10.1f);
+//    }
 }
--- MyAnnotation.java
+++ MyAnnotation.java
@@ -1,13 +1,17 @@
 package com.lous;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.*;
 
 /**
  * 自定义注释
  */
+//用于生成文档
+@Documented
 //表示该注解的作用范围在运行时存在
 @Retention(RetentionPolicy.RUNTIME)
+//用于表示注解的应用范围(类型,方法,属性,构造器,参数,局部变量)
+@Target(ElementType.TYPE)
+@Inherited
 public @interface MyAnnotation {
     //自定义变量
     public String name();
